3da26aeb3bd0d041c1789d36c827c59cdb6dc68a
Pepino <c.gutierrez.j196@edu.utc.mx>
2024-02-21 23:07:46 -0600

    este commit es sobre lo que estudio en javascript

---
+++ Gutierrez Juarez Cristian/FuncionFind/01-tipos/03-constantes.js
@@ -0,0 +1,3 @@
+let nombre = 'hola mundo'; //let puede cambiar su valor sin la nesecidad de volver a poner let
+nombre = 'chanchito feliz';
+console.log(nombre);
---
+++ Gutierrez Juarez Cristian/FuncionFind/Find.js
@@ -0,0 +1,8 @@
+//El método find()devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find((element) => element > 10);
+
+console.log(found);
+
